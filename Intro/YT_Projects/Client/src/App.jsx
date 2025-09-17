9de070e6a910a5c97e58a3588334f11d8f1a068f
Dhruv Soni <dhruvsonimain@gmail.com>
2025-09-17 19:33:56 +0530

    Added tailwind and daisyui and hot toasat etc... learned good ui settings

--- Intro/YT_Projects/Client/src/App.jsx
+++ Intro/YT_Projects/Client/src/App.jsx
@@ -1,21 +1,20 @@
-import {Route, Routes} from 'react-router'
 import React from 'react'
-import toast from 'react-hot-toast'
+import { Route, Router } from 'react-router'
 
-import homePage from './pages/Homepage.jsx'
-import createPage from './pages/Create.jsx'
-import detailsPage from './pages/Details.jsx'
+import homePage from './Pages/homePage'
+import detailPage from './Pages/detailPage'
+import createPage from './Pages/createPage'
 
-function App() {
+const App = () => {
   return (
-    <>
-    <button>Click Me</button>
-      <Routes>
+    <div data-theme='forest'>
+      <Router>
         <Route path='/' element={<homePage />} />
+        <Route path='/note/:id' element={<detailPage />} />
         <Route path='/create' element={<createPage />} />
-        <Route path='/note/:id' element={<detailsPage />} />
-      </Routes>
-    </>
+      </Router>
+    </div>
   )
 }
-export default App;
+
+export default App
