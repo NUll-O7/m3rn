cc58f8727c16b64555dd9825e729af42457c6df2
Dhruv Soni <dhruvsonimain@gmail.com>
2025-10-12 05:52:44 +0530

    Express Basics

--- Intro/Class/Self/InClass/class1/index.js
+++ Intro/Class/Self/InClass/class1/index.js
@@ -1,8 +1,28 @@
 const http = require('http');
+const fs = require('fs');
 
 http.createServer((req,res)=>{
-    console.log("Request received");
-    res.end("Hello World from Dhruv's server");
+    let log = `${Date.now()} : ${req.url} New Req recieved\n`;
+    console.log(log);
+
+    fs.appendFile('log.txt',log,function(err,data){
+        console.log("Log Updated");
+    });
+
+    switch(req.url){
+        case '/':
+            res.end('Welcome to Home Page');
+            break;
+        case '/about':
+            res.end('Welcome to About Page');
+            break;
+        case '/contact':
+            res.end('Welcome to Contact Page');
+            break;
+        default:
+            res.end('404 Page Not Found');
+            break;
+    }
 }).listen(8000,()=>{
     console.log("Server is listening on port 8000");
 });
